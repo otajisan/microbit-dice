7a54213537d456798de758aa7dbd0ec72cab666a
Taji <taji.taji.otajisan@gmail.com>
2021-03-31 12:41:00 +0900

    fix: make scratch image

--- main.py
+++ main.py
@@ -1,53 +1,55 @@
+dice = 0
+
 def on_button_pressed_a():
+    global dice
+    music.play_tone(262, music.beat(BeatFraction.EIGHTH))
     dice = randint(1, 6)
-
     if dice == 1:
-        basic.show_animation("""
-        . . . . .
-        . . . . .
-        . . # . .
-        . . . . .
-        . . . . .
-        """)
+        basic.show_leds("""
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            """)
     elif dice == 2:
-        basic.show_animation("""
-        . . . . #
-        . . . . .
-        . . . . .
-        . . . . .
-        # . . . .
-        """)
+        basic.show_leds("""
+            . . . . #
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . .
+            """)
     elif dice == 3:
-        basic.show_animation("""
-        . . . . #
-        . . . . .
-        . . # . .
-        . . . . .
-        # . . . .
-        """)
+        basic.show_leds("""
+            . . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . .
+            """)
     elif dice == 4:
-        basic.show_animation("""
-        # . . . #
-        . . . . .
-        . . . . .
-        . . . . .
-        # . . . #
-        """)
+        basic.show_leds("""
+            # . . . #
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . #
+            """)
     elif dice == 5:
-        basic.show_animation("""
-        # . . . #
-        . . . . .
-        . . # . .
-        . . . . .
-        # . . . #
-        """)
+        basic.show_leds("""
+            # . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . #
+            """)
     elif dice == 6:
-        basic.show_animation("""
-        # . . . #
-        . . . . .
-        # . . . #
-        . . . . .
-        # . . . #
-        """)
-
+        basic.show_leds("""
+            # . . . #
+            . . . . .
+            # . . . #
+            . . . . .
+            # . . . #
+            """)
 input.on_button_pressed(Button.A, on_button_pressed_a)
--- main.ts
+++ main.ts
@@ -1,54 +1,56 @@
 let dice = 0
-input.onButtonPressed(Button.A, function () {
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    
     music.playTone(262, music.beat(BeatFraction.Eighth))
     dice = randint(1, 6)
     if (dice == 1) {
-        basic.showAnimation(`
-        . . . . .
-        . . . . .
-        . . # . .
-        . . . . .
-        . . . . .
-        `)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
     } else if (dice == 2) {
-        basic.showAnimation(`
-        . . . . #
-        . . . . .
-        . . . . .
-        . . . . .
-        # . . . .
-        `)
+        basic.showLeds(`
+            . . . . #
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . .
+            `)
     } else if (dice == 3) {
-        basic.showAnimation(`
-        . . . . #
-        . . . . .
-        . . # . .
-        . . . . .
-        # . . . .
-        `)
+        basic.showLeds(`
+            . . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . .
+            `)
     } else if (dice == 4) {
-        basic.showAnimation(`
-        # . . . #
-        . . . . .
-        . . . . .
-        . . . . .
-        # . . . #
-        `)
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . #
+            `)
     } else if (dice == 5) {
-        basic.showAnimation(`
-        # . . . #
-        . . . . .
-        . . # . .
-        . . . . .
-        # . . . #
-        `)
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . #
+            `)
     } else if (dice == 6) {
-        basic.showAnimation(`
-        # . . . #
-        . . . . .
-        # . . . #
-        . . . . .
-        # . . . #
-        `)
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            # . . . #
+            . . . . .
+            # . . . #
+            `)
     }
+    
 })
